97bf65ae2c6c1dbcfc3d6920fbacc93d9cf8f632
Louis Tur <louis.tur@gmail.com>
2016-10-20 00:49:01 -0400

    updates tests

--- AC3.2-InstaCats-2Tests/AC3_2_InstaCats_2Tests.swift
+++ AC3.2-InstaCats-2Tests/AC3_2_InstaCats_2Tests.swift
@@ -31,6 +31,8 @@ class AC3_2_InstaCats_2Tests: XCTestCase {
         super.tearDown()
     }
     
+    
+    // MARK: - InstaCats test -
     func testInitializerOfInstaCat() {
         let testInstaCat: InstaCat = InstaCat(name: testName, id: testID, instagramURL: testURL)
         
@@ -46,40 +48,52 @@ class AC3_2_InstaCats_2Tests: XCTestCase {
         XCTAssertTrue(testInstaCat.description == expectedDescription)
     }
     
-    func testGetResourceURLFromFilename() {
-        let testFileName: String = "test.json"
-        let testFileURL = testInstaCatTableVC.getResourceURL(from: testFileName)
-        XCTAssertNotNil(testFileURL, "getResourceURL(from:) should return a non-nil URL for a valid file")
-        
-        let invalidFileName: String = "testing.json"
-        let invalidTestFileURL = testInstaCatTableVC.getResourceURL(from: invalidFileName)
-        XCTAssertNil(invalidTestFileURL, "getResourceURL(from:) should return nil for a file that does not exist")
-        
-        let malformedFileName: String = "testedjson"
-        let malformedFileURL = testInstaCatTableVC.getResourceURL(from: malformedFileName)
-        XCTAssertNil(malformedFileURL, "getResourceURL(from:) should return nil for an improperly formatted filename parameter")
-        
-    }
-    
-    func testGetDataFromURL() {
-        let testFileURL = testInstaCatTableVC.getResourceURL(from: testFileName)
-        let testData = testInstaCatTableVC.getData(from: testFileURL!)
-        XCTAssertNotNil(testData, "getData(from:) should return a non-nil URL for a valid file")
-    }
-    
     func testGettingInstaCatsFromData() {
-        guard let testFileURL = testInstaCatTableVC.getResourceURL(from: testFileName),
-            let testFileData = testInstaCatTableVC.getData(from: testFileURL),
-            let testFileInstaCats = testInstaCatTableVC.getInstaCats(from: testFileData)
-        else {
+        guard let testCats = InstaCatFactory.makeInstaCats(fileName: testFileName) else {
             XCTFail()
             return
         }
         
-        XCTAssertTrue(testFileInstaCats.count == 3)
-        XCTAssertTrue(testFileInstaCats[0].name == "Nala Cat")
-        XCTAssertTrue(testFileInstaCats[1].catID == 002)
-        XCTAssertTrue(testFileInstaCats[2].instagramURL == URL(string: "https://www.instagram.com/grump_cat_/?hl=en"))
+        XCTAssertTrue(testCats.count == 3)
+        XCTAssertTrue(testCats[0].name == "Nala Cat")
+        XCTAssertTrue(testCats[1].catID == 002)
+        XCTAssertTrue(testCats[2].instagramURL == URL(string: "https://www.instagram.com/grump_cat_/?hl=en"))
     }
 
+    
+    // MARK: - InstaDogs Tests - 
+    func testInitializerOfInstaDog() {
+        let components = self.testFileName.components(separatedBy: ".")
+        guard let fileURL = Bundle.main.url(forResource: components.first, withExtension: components.last) else {
+            XCTFail()
+            return
+        }
+        
+        if let validData: Data = try? Data(contentsOf: fileURL) {
+            if let instaDogs: [InstaDog] = InstaDogFactory.manager.getInstaDogs(from: validData) {
+                
+                guard let onlyInstaDog = instaDogs.first else {
+                    XCTFail("InstaDogFactory should generate the correct number of instaDogs from test data. Expected: 1, Actual: \(instaDogs.count)")
+                    return
+                }
+                
+                // properties
+                XCTAssertTrue(onlyInstaDog.name == "Men's Wear Dog")
+                XCTAssertTrue(onlyInstaDog.dogID == 001)
+                XCTAssertTrue(onlyInstaDog.instagramURL == URL(string: "https://www.instagram.com/mensweardog/")!)
+                XCTAssertTrue(onlyInstaDog.imageName == "mens_wear_dog.jpg")
+                XCTAssertTrue(onlyInstaDog.followers > 0)
+                XCTAssertTrue(onlyInstaDog.following > 0)
+                XCTAssertTrue(onlyInstaDog.numberOfPosts > 0)
+                
+                // functions
+                XCTAssertTrue(onlyInstaDog.formattedStats() == "Posts: \(onlyInstaDog.numberOfPosts)   Followers: \(onlyInstaDog.followers)   Following:\(onlyInstaDog.following)")
+                
+                XCTAssertNotNil(onlyInstaDog.profileImage())
+                XCTAssertTrue(onlyInstaDog.profileImage() == UIImage(named: onlyInstaDog.imageName))
+            }
+        }
+        
+        
+    }
 }
